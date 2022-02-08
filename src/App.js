a5ec95310e51fd5579f71b933f0fe05c46d842d7
Milad <milad.kareem@gmail.com>
2022-02-08 05:37:59 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      fetch(
+        "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-01-01&limit=20"
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data.features.map(earthquake => ({
+              id: earthquake.id, 
+              place: earthquake.properties.place, 
+              magnitude: earthquake.properties.mag, 
+              time: (new Date(earthquake.properties.time).toLocaleString()),
+              })
+          ))
+        }).catch(e=>console.log(e));
+    }, []);
+  
+    return <div className="App">
+      {data.map(earthquake => {
+        return <div  key={earthquake.id} className='card'>
+            <div className="mag" style={{ backgroundColor: `rgba(255, 0, 0, ${earthquake.magnitude*0.2})`}}>
+                <h1>
+                  {earthquake?.magnitude.toFixed(2)}
+                </h1>
+            </div>
+            <div className="cont">
+              <h2>{earthquake?.place}</h2>
+              <h3>{earthquake?.time}</h3>
+  
+            </div>
+        </div>
+      })}
+    </div>;
+  }
+  
+  export default App;
+  
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('Home component',()=>{
+    const resolved = async()=>Promise.resolve({
+        json: async()=>{ return Promise.resolve({
+            features: [
+                {
+                    id:'001',
+                    properties:{
+                        place: 'test place',
+                        mag: 0.5,
+                        time: Date.now()
+                    }
+                }
+            ]
+        })}
+    })
+
+
+    test('when data is received the "test place" is on screen', async()=>{
+        // arrange
+        // window.fetch.mockResolvedValueOnce(resolved)
+        window.fetch = jest.fn(resolved);
+        render(<App/>)
+
+
+        const hElement = await screen.findByText('test place',{}, {timeout: 3000} )
+        expect(hElement).toBeInTheDocument()
+
+    })
+
+    test('when data is not received the "test place" is not on screen', async()=>{
+        // arrange
+        window.fetch.mockRejectedValueOnce('rejected')
+        // window.fetch = jest.fn(()=>Promise.reject('rejected'));
+        render(<App/>)
+
+        await waitFor(()=>{
+            expect(screen.queryByText('test place')).not.toBeInTheDocument()
+        
+        })
+        expect(window.fetch).toBeCalledTimes(1)
+        
+
+    })
+})
